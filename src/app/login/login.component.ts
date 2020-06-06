8771ddd9fe3ca7b58b1567162e7965f49a3bbc37
jjvduncan <jjv.duncan@yavirac.edu.ec>
2020-06-06 15:02:52 -0500

    app

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,7 +15,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private router: Router) { }
 username: string;
-password: string;
+password: string; 
   ngOnInit() {
   }
   login() : void {
